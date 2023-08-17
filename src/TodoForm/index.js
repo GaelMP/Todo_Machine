2b87711f930a24c36270443606669f6210befedd
GaelMP <gaelcabrera7112010@gmail.com>
2023-08-17 12:33:44 -0500

    Todo_Machine - V_1.7

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -9,7 +9,7 @@ function TodoForm() {
         setOpenModal
     } = React.useContext(TodoContext)
 
-    const maxLength = 50;
+    const maxLength = 43;
 
     const onChange = (event) => {
         const newText = event.target.value;
