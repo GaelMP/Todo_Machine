28ae375aa3f300eaa8c09b92dfff9d515476cef8
GaelMP <gaelcabrera7112010@gmail.com>
2023-08-20 10:27:34 -0500

    Todo_Machine - V_2.0

--- src/Alerts/index.js
+++ src/Alerts/index.js
@@ -12,7 +12,24 @@ function Alerts(props) {
 
             {props.error && <h1>😨desesperate huvo un error😨</h1>}
 
-            {props.loading && <div className="loader"></div>}
+            {props.loading && <div aria-label="Orange and tan hamster running in a metal wheel" role="img" class="wheel-and-hamster">
+	                             <div class="wheel"></div>
+	                             <div class="hamster">
+		                           <div class="hamster__body">
+			                        <div class="hamster__head">
+	                		            <div class="hamster__ear"></div>
+			                            <div class="hamster__eye"></div>
+			             	            <div class="hamster__nose"></div>
+			                        </div>
+			                        <div class="hamster__limb hamster__limb--fr"></div>
+			                        <div class="hamster__limb hamster__limb--fl"></div>
+                                    <div class="hamster__limb hamster__limb--br"></div>
+                                    <div class="hamster__limb hamster__limb--bl"></div>
+                                    <div class="hamster__tail"></div>
+		                         </div>
+	                          </div>
+	                          <div class="spoke"></div>
+                         </div>}
 
             {(!props.loading && !props.searchedTodos.length) && <h1 className="create" onClick={onClickButton}>➕Crea tu primera tarea aquí➕</h1>}
 
